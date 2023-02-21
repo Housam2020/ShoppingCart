cc8a32f7e5ea78a27a9b6f89fa1394a311e7d9a3
housam2020 <housam4508@gmail.com>
2023-02-20 20:50:31 -0500

    Add project files.

---
+++ ShopOnline.API/Entities/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopOnline.API.Entities
+{
+    public class ProductCategory
+    {
+        //designated as the primary key of this entity automatically by entity-framework-core
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+    }
+}
